dcc155d1f168c570fd84484432005406c49392e2
rkdlqzz <rkdlqzz081@gmail.com>
2022-04-10 16:01:20 +0900

    프로젝트 생성

---
+++ APPLE/app/src/main/java/com/example/apple/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.apple;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
