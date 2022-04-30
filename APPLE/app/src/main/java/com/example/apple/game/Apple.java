573f7a68504d5a62dc198a9b4f1f0f757223a317
rkdlqzz <rkdlqzz081@gmail.com>
2022-04-30 20:27:39 +0900

    캐릭터(Apple)의 이동방식 변경 (조이스틱 입력에 따른 이동 및 회전)

--- APPLE/app/src/main/java/com/example/apple/game/Apple.java
+++ APPLE/app/src/main/java/com/example/apple/game/Apple.java
@@ -3,6 +3,7 @@ package com.example.apple.game;
 import android.graphics.Canvas;
 
 import com.example.apple.R;
+import com.example.apple.framework.Joystick;
 import com.example.apple.framework.Metrics;
 import com.example.apple.framework.Sprite;
 
@@ -11,15 +12,15 @@ public class Apple extends Sprite {
 
     private float angle;
     private float dx, dy;
-    private float tx, ty;
+    private final Joystick joystick;
 
-    public Apple(float x, float y) {
+    public Apple(float x, float y, Joystick joystick) {
         super(x, y, R.dimen.apple_radius, R.mipmap.apple_red);
-        setTargetPosition(x, y);
-        angle = -(float) (Math.PI / 2);
+        this.joystick = joystick;
     }
 
     public void draw(Canvas canvas) {
+        setDstRectWithRadius();
         canvas.save();
         canvas.rotate((float) (angle * 180 / Math.PI) + 90, x, y);
         canvas.drawBitmap(bitmap, null, dstRect, null);
@@ -27,44 +28,13 @@ public class Apple extends Sprite {
     }
 
     public void update() {
-        if (dx == 0 && dy == 0)
-            return;
-
-        boolean arrivedX = false;
-        if ((dx > 0 && x + dx > tx) || (dx < 0 && x + dx < tx)) {
-            dx = tx - x;
-            x = tx;
-            arrivedX = true;
-        } else {
-            x += dx;
-        }
-
-        boolean arrivedY = false;
-        if ((dy > 0 && y + dy > ty) || (dy < 0 && y + dy < ty)) {
-            dy = ty - y;
-            y = ty;
-            arrivedY = true;
-        } else {
-            y += dy;
-        }
-
-        dstRect.offset(dx, dy);
-
-        if (arrivedX) {
-            this.dx = 0;
-        }
-        if (arrivedY) {
-            this.dy = 0;
-        }
-    }
-
-    public void setTargetPosition(float tx, float ty) {
-        this.tx = tx;
-        this.ty = ty;
-        angle = (float) Math.atan2(ty - y, tx - x);
+        float frameTime = MainGame.getInstance().frameTime;
         float speed = Metrics.size(R.dimen.apple_red_speed);
-        float dist = speed * MainGame.getInstance().frameTime;
-        dx = (float) (dist * Math.cos(angle));
-        dy = (float) (dist * Math.sin(angle));
+
+        dx = (float) joystick.GetActuatorX() * speed * frameTime;
+        x += dx;
+        dy = (float) joystick.GetActuatorY() * speed * frameTime;
+        y += dy;
+        angle = (float) joystick.GetAngleRadian();
     }
 }
