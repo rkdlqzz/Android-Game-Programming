3e05b4c04acd757e8aac1133b3d1fa0e7a445e07
rkdlqzz <rkdlqzz081@gmail.com>
2022-04-30 20:24:04 +0900

    Joystick 클래스 생성 (입력에 따른 이동거리, 중심으로부터의 각도 설정)

---
+++ APPLE/app/src/main/java/com/example/apple/framework/Joystick.java
@@ -0,0 +1,93 @@
+package com.example.apple.framework;
+
+import android.graphics.Canvas;
+
+import com.example.apple.R;
+
+public class Joystick implements GameObject {
+    private static final String TAG = Joystick.class.getSimpleName();
+    private Sprite outCircle;
+    private Sprite inCircle;
+    private float outCircleRadius;
+    private float inCircleRadius;
+    private float outCircleCY, outCircleCX;
+    private double touchDistFromJoystick;
+    private boolean isPressed;
+    private double actuatorX, actuatorY;
+    private double angle;
+
+    public Joystick(float x, float y) {
+        outCircleRadius = Metrics.size(R.dimen.joystick_bg_radius);
+        inCircleRadius = Metrics.size(R.dimen.joystick_button_radius);
+
+        outCircle = new Sprite(x, y, outCircleRadius, R.mipmap.joystick_background);
+        inCircle = new Sprite(x, y, inCircleRadius, R.mipmap.joystick_button);
+
+        outCircleCX = x;
+        outCircleCY = y;
+
+        angle = -90.0f / 180.0f * Math.PI;  // 초기 각도 설정, -90도를 라디안으로
+    }
+
+
+    @Override
+    public void update() {
+        inCircle.x = (float) (outCircleCX + actuatorX * outCircleRadius);
+        inCircle.y = (float) (outCircleCY + actuatorY * outCircleRadius);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        outCircle.draw(canvas);
+        inCircle.setDstRectWithRadius();
+        inCircle.draw(canvas);
+    }
+
+    public void SetIsPressed(double touchX, double touchY) {
+        touchDistFromJoystick = Math.sqrt(
+                Math.pow(outCircleCX - touchX, 2) + Math.pow(outCircleCY - touchY, 2));
+
+        if (touchDistFromJoystick < outCircleRadius) isPressed = true;
+    }
+
+    public void SetIsPressed(boolean isPressed) {
+        this.isPressed = isPressed;
+    }
+
+    public boolean GetIsPressed() {
+        return isPressed;
+    }
+
+    public void SetActuator(double touchX, double touchY) {
+        double dx = touchX - outCircleCX;
+        double dy = touchY - outCircleCY;
+        double distance = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+
+        if (distance < outCircleRadius) {
+            actuatorX = dx / outCircleRadius;
+            actuatorY = dy / outCircleRadius;
+        } else {
+            actuatorX = dx / distance;
+            actuatorY = dy / distance;
+        }
+
+        angle = Math.atan2(actuatorY, actuatorX);
+    }
+
+    public double GetActuatorX() {
+        return actuatorX;
+    }
+
+    public double GetActuatorY() {
+        return actuatorY;
+    }
+
+    public void ResetActuator() {
+        actuatorX = 0.0;
+        actuatorY = 0.0;
+    }
+
+    public double GetAngleRadian() {
+        return angle;
+    }
+}
