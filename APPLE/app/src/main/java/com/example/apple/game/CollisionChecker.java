da116f6fd690f046e1a0ef9b32504f5944af092f
rkdlqzz <rkdlqzz081@gmail.com>
2022-05-01 20:38:59 +0900

    캐릭터와 Enemy의 총돌검사 및 충돌처리(CollisionChecker 사용, 임시로 충돌 시 enemy 제거되도록 설정)

---
+++ APPLE/app/src/main/java/com/example/apple/game/CollisionChecker.java
@@ -0,0 +1,47 @@
+package com.example.apple.game;
+
+import android.graphics.Canvas;
+import android.util.Log;
+
+import com.example.apple.framework.CollisionHelper;
+import com.example.apple.framework.GameObject;
+
+import java.util.ArrayList;
+
+public class CollisionChecker implements GameObject {
+    private static final String TAG = CollisionChecker.class.getSimpleName();
+
+    @Override
+    public void update() {
+        MainGame game = MainGame.getInstance();
+        ArrayList<GameObject> players = game.objectsAt(MainGame.Layer.player);
+        ArrayList<GameObject> enemies = game.objectsAt(MainGame.Layer.enemy);
+
+        for (GameObject o1 : enemies) {
+            if (!(o1 instanceof Enemy)) {
+                continue;
+            }
+            Enemy enemy = (Enemy) o1;
+            boolean collided = false;
+            for (GameObject o2 : players) {
+                if (!(o2 instanceof Apple)) {
+                    continue;
+                }
+                Apple player = (Apple) o2;
+                if (CollisionHelper.collides(enemy, player)) {
+                    game.remove(enemy);
+                    collided = true;
+                    break;
+                }
+            }
+            if (collided) {
+                continue;
+            }
+        }
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+
+    }
+}
