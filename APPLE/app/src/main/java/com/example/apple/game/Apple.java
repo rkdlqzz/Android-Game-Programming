bb33d038f00e340b813cd59e2c9690610dee4ee8
rkdlqzz <rkdlqzz081@gmail.com>
2022-05-02 14:44:03 +0900

    캐릭터가 speed up 아이템 획득 시 효과 구현(이동속도 증가, 색상 변화)

--- APPLE/app/src/main/java/com/example/apple/game/Apple.java
+++ APPLE/app/src/main/java/com/example/apple/game/Apple.java
@@ -1,9 +1,11 @@
 package com.example.apple.game;
 
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.RectF;
 
 import com.example.apple.R;
+import com.example.apple.framework.BitmapPool;
 import com.example.apple.framework.CircleCollidable;
 import com.example.apple.framework.Joystick;
 import com.example.apple.framework.Metrics;
@@ -16,20 +18,52 @@ public class Apple extends Sprite implements CircleCollidable {
     private float dx, dy;
     private final Joystick joystick;
 
+    private float speed;
+    public float durationSpeedUp;
+    private Bitmap bitmapRedApple;
+    private Bitmap bitmapGreenApple;
+
     public Apple(float x, float y, Joystick joystick) {
         super(x, y, R.dimen.apple_radius, R.mipmap.apple_red);
         this.joystick = joystick;
+        speed = Metrics.size(R.dimen.apple_red_speed);
+
+        durationSpeedUp = 0.0f;
+        bitmapRedApple = BitmapPool.get(R.mipmap.apple_red);
+        bitmapGreenApple = BitmapPool.get(R.mipmap.apple_green);
     }
 
     public void update() {
         float frameTime = MainGame.getInstance().frameTime;
-        float speed = Metrics.size(R.dimen.apple_red_speed);
+
+        updateSpeedUp(frameTime);
 
         dx = (float) joystick.GetActuatorX() * speed * frameTime;
         x += dx;
         dy = (float) joystick.GetActuatorY() * speed * frameTime;
         y += dy;
         angle = (float) joystick.GetAngleRadian();
+    }
+
+    private void updateSpeedUp(float frameTime) {
+        if (durationSpeedUp <= 0.0f) {  // 아이템 지속시간 끝나면 speed, bitmap 초기화
+            speed = Metrics.size(R.dimen.apple_red_speed);
+            durationSpeedUp = 0.0f;
+            if (bitmap == bitmapGreenApple)
+                bitmap = bitmapRedApple;
+            return;
+        } else {
+            durationSpeedUp -= frameTime;
+            speed = Metrics.size(R.dimen.apple_green_speed);
+
+            if (durationSpeedUp < 1.5f) {   // SpeedUp 지속시간이 1초 남은 경우 깜빡이도록
+                if (bitmap == bitmapRedApple)
+                    bitmap = bitmapGreenApple;
+                else bitmap = bitmapRedApple;
+            } else {
+                bitmap = bitmapGreenApple;
+            }
+        }
     }
 
     public void draw(Canvas canvas) {
@@ -58,5 +92,19 @@ public class Apple extends Sprite implements CircleCollidable {
     @Override
     public float getRadius() {
         return radius;
+    }
+
+    public void getItem(int type, float duration) {
+        switch (type) {
+            case 0:     // speed up
+                durationSpeedUp = duration;
+                break;
+            case 1:     // leaf bomb
+                break;
+            case 2:     // wood shield
+                break;
+            default:
+                break;
+        }
     }
 }
