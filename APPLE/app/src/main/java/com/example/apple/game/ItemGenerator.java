6f05633a853945aa92ea98f701ad15fce6412b7e
rkdlqzz <rkdlqzz081@gmail.com>
2022-05-22 00:17:22 +0900

    아이템 스폰방식 변경

--- APPLE/app/src/main/java/com/example/apple/game/ItemGenerator.java
+++ APPLE/app/src/main/java/com/example/apple/game/ItemGenerator.java
@@ -11,32 +11,28 @@ import java.util.Random;
 
 public class ItemGenerator implements GameObject {
     private static final String TAG = ItemGenerator.class.getSimpleName();
-    private static final float INITIAL_SPAWN_INTERVAL = 5.0f;   // 초기 스폰 간격
-    private static final int INITIAL_MAX_ITEM = 3;    // 아이템의 최대 수 초기값
-    private static final int INCREMENT_MAX_ITEM = 1;    // 스테이지 증가 시 증가하는 아이템의 최대 수
-    private float spawnInterval;
+    private static final int[] MAX_ITEM = {2, 3, 3}; // 한화면에 존재할 수 있는 아이템의 최대 수 (스테이지별)
+    private static final float[] SPAWN_INTERVAL = {5.0f, 4.0f, 4.0f};   // 스폰 간격 (스테이지별)
     private float fallSpeed;
     private float elapsedTime;
-    private int maxItem;   // 한화면에 존재할 수 있는 아이템의 최대 수
 
     public ItemGenerator() {
-        this.spawnInterval = INITIAL_SPAWN_INTERVAL;
         this.fallSpeed = Metrics.size(R.dimen.item_fall_speed);
-        this.maxItem = INITIAL_MAX_ITEM;
     }
 
     @Override
     public void update() {
-        float frameTime = MainGame.getInstance().frameTime;
-        //Log.d(TAG, "NumOfItem : " + MainGame.getInstance().objectsAt(MainGame.Layer.item).size());
+        MainGame game = MainGame.getInstance();
+        float frameTime = game.frameTime;
+        Log.d(TAG, "NumOfItem : " + game.objectsAt(MainGame.Layer.item).size());
 
         // maxItem 이상은 item spawn하지 않도록
-        if (MainGame.getInstance().objectsAt(MainGame.Layer.item).size() >= maxItem) return;
+        if (game.objectsAt(MainGame.Layer.item).size() >= MAX_ITEM[game.stage.get() - 1]) return;
 
         elapsedTime += frameTime;
-        if (elapsedTime > spawnInterval) {
+        if (elapsedTime > SPAWN_INTERVAL[game.stage.get() - 1]) {
             spawn();
-            elapsedTime -= spawnInterval;
+            elapsedTime -= SPAWN_INTERVAL[game.stage.get() - 1];
         }
     }
 
@@ -46,7 +42,7 @@ public class ItemGenerator implements GameObject {
         int type, bitmapResId = 0;
 
         // x
-        x = Metrics.width / 10.0f + random.nextInt((int) (Metrics.width * 0.8f));   // width의 0.1~0.9 사이의 x값
+        x = Metrics.width * 0.1f + random.nextInt((int) (Metrics.width * 0.8f));   // width의 0.1~0.9 사이의 x값
 
         // type, bitmapResId
         type = random.nextInt(3);
